e85b0f96a4c0a64394e0a38cdcd5cea6596bc71a
gabriel natan <g.souza@grouplinknetwork.com>
2022-08-21 15:21:45 -0300

    change varibles css

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -5,10 +5,12 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    font-family: 'Inter', sans-serif;
   }
 
   :root{
     --primary-color: #30A6FB;
+    --primary-hover-color: #30A6FB80;
     --secondary-color: #F5F7FE;
     --font-color:#525252;
     --font-color-secondary:#424242;
@@ -19,6 +21,7 @@ const GlobalStyle = createGlobalStyle`
     --base-purple:#840FD1;
     --base-light-blue:#12AFF0;
 
+    --font-size-primary-lg:18px;
     --font-size-primary:16px;
     --font-size-primary-sm:14px;
     --font-size-secondary-sm:12px;
@@ -26,6 +29,7 @@ const GlobalStyle = createGlobalStyle`
     --font-size-title-sm:60px;
     --font-size-subtitle:30px;
 
+    --transition: 150ms;
   }
 
 `;
